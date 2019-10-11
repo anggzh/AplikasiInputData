0fd8bd60d8ac920fabe708e99f8e993f1ca50f4e
anggzh <45726270+anggzh@users.noreply.github.com>
2019-10-11 10:25:13 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,11 @@
+﻿namespace AplikasiInputDataMahasiswa
+{
+    internal class Mahasiswa
+    {
+        public int Nilai { get; internal set; }
+        public string Kelas { get; internal set; }
+        public string Nama { get; internal set; }
+        public string Nim { get; internal set; }
+        public string Huruf { get; internal set; }
+    }
+}
